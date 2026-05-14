cb671b2ba591e5739f5ce7118fb4279cdbbee544
Milu-BD <alhifazatmilu@gmail.com>
2026-05-14 19:02:12 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -170,7 +170,7 @@ export default function Home() {
     backgroundPosition: "center",
     backgroundSize: "contain",
 
-    opacity: 0.7,
+    opacity: 0.63,
 
     zIndex: 0,
     pointerEvents: "none",
